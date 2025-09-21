d975be07c52c82fe7ad472b394abbf2e98a4e962
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-09-20 22:46:27 -0300

    Finalizacao projeto

---
+++ src/main/java/com/devsuperior/desafio/controllers/hendlers/ClientController.java
@@ -0,0 +1,51 @@
+package com.devsuperior.desafio.controllers.hendlers;
+
+import com.devsuperior.desafio.dto.ClientDTO;
+import com.devsuperior.desafio.services.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(clientService.findAll(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> save(@Valid @RequestBody ClientDTO clientDTO) {
+        clientDTO = clientService.insert(clientDTO);
+        URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(clientDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(clientDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO clientDTO) {
+        clientDTO = clientService.update(id, clientDTO);
+        return ResponseEntity.ok(clientDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Isaac Bernardo Lorenzo Corte Real', '833.427.385-10', 3200.34, '1986-01-16', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Aline Jaqueline Lavínia', '482.785.353-37', 6500.90, '1966-10-16', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Marcos Davi Corte Real', '727.047.854-65', 4560.90, '1967-09-16', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Sarah Mirella Elaine Cardoso', '262.368.090-62', 4500.34, '1986-01-16', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Luzia Analu Daiane', '219.804.626-15', 2300.45, '1988-01-11', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Cláudio Anderson Cardoso', '307.433.588-21', 2010.24, '1990-02-23', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Daniela Fabiana Sarah Monteiro', '911.136.464-54', 4200.44, '1989-05-16', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Jaqueline Josefa', '929.290.447-72', 6200.38, '1985-05-23', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date) VALUES ('Arthur Marcos Vinicius Rocha', '057.107.639-42', 1200.34, '1993-01-11');
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Aparecida Ayla Vitória', '106.088.224-85', 1540.33, '1956-01-18', 5);
+
